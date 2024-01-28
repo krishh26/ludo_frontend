67f49513b4e955d394fd33de2925ce6b6aa0449a
darshan <darshandumaraliya@gmail.com>
2024-01-29 01:59:10 +0530

    profile related change.

--- src/app/pages/home/profile/profile.component.ts
+++ src/app/pages/home/profile/profile.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LocalStorageService } from 'src/app/services/local-storage/local-storage.service';
+import { WalletWithdrawServiceService } from 'src/app/services/wallet-withdraw-service/wallet-withdraw-service.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +10,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent {
+  loginUser!: any;
+  totalAmount : any = 0;
+
+  full_name: FormControl = new FormControl('');
+
+  constructor(
+    private router: Router,
+    private localStorageService: LocalStorageService,
+    private walletService : WalletWithdrawServiceService
+  ) {
+    this.loginUser = this.localStorageService.getLogger();
+    this.walletService.userTotalAmount$.subscribe((amount) => this.totalAmount = amount);
+  }
 
 }
